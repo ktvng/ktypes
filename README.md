d091b654093edb04d5ad6d0ec1e3f0c63bef32c4
Kevin Tang <just.ktang@gmail.com>
2021-04-29 12:52:00 -0400

    add PyPi packaging files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KnownTypes (KTypes)
 A high-level, type-theoretic data annotation language built into Python 3
+
+## Overview
+KTypes is a library built to allow for type theortic code constructions and type safe data conversions alongside the standard Python type system.
--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='ktypes',
-    packages=find_packages(include="ktypes"),
-    version='0.0.1',
+    package_dir= {'ktypes/ktypes': 'ktypes'},
+    packages=find_packages(include=['ktypes']),
+    version='0.0.3',
     description='Type Theoretic Data Annotations',
     author='Kevin Tang',
     license='MIT',
